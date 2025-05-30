9b53dc51a6c446d01c53e70c8853f394927eb463
hehongtao <hehongtao@kandu.com.cn>
2025-05-30 19:43:55 +0800

    修改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   server: {
     port: 3000,
     host: '0.0.0.0',
     proxy: {
-      '/basapi': {
+      '/api': {
         target: 'http://localhost:8080',
         //支持所有ip访问
 
